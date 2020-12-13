5d4df98d0b34e358795dbafe161e4e4dd3bc0a31
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-12-13 14:34:37 -0200

    CadastroController.php

---
+++ app/Controllers/CadastroController.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "app/Models/Noticia.php";
+
+class CadastroController
+{
+    public function cadastro()
+    {
+        //verifica se exite os posts titulo, categoria e conteudo
+        if(isset($_POST['titulo']) && isset($_POST['categoria']) && isset($_POST['conteudo'])){
+
+            $titulo    = $_POST['titulo'];
+            $categoria = $_POST['categoria'];
+            $conteudo  = $_POST['conteudo'];
+
+            //verifica se as variaveis não estão vazias
+            if(!empty($titulo) && !empty($categoria) && !empty($conteudo)){
+         
+                $noticia = new Noticia();
+                $noticia->setTitulo($titulo);
+                $noticia->setCategoria($categoria);
+                $noticia->setConteudo($conteudo);
+                //o metodo cadasra, cadastra os dados
+                if($noticia->cadastra()){
+                    echo "<script>alert('Cadastrado com sucesso!')</script>"; 
+                }else{
+                    echo "<script>alert('Erro ao cadastrar!')</script>"; 
+                }
+
+            }else{
+                echo "<script>alert('Porfavor preencha todos os campos!')</script>";
+            }
+        }
+    }
+}
