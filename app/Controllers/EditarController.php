15f45dfb17b145d0edeec9e4d9c4d2302992a412
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-12-21 12:19:15 -0200

    EditarController.php

--- app/Controllers/EditarController.php
+++ app/Controllers/EditarController.php
@@ -2,40 +2,41 @@
 
 require_once "app/Models/Noticia.php";
 
-        $noticia = null;
-        
-        if(isset($_GET['id'])){
-            $n = new Noticia();
-            $n->setId($_GET['id']);
-            $noticia = $n->selectUniNoticia() ;
+$noticia = null;
+
+//verifica se existe o get id
+if(isset($_GET['id'])){
+    $n = new Noticia();
+    $n->setId($_GET['id']);
+    $noticia = $n->selectUniNoticia();
+}
+
+//verifica se existe os post titulo e conteudo
+if(isset($_POST['titulo']) && isset($_POST['conteudo'])){
+
+    $id       = $_GET['id'];
+    $titulo   = $_POST['titulo'];
+    $conteudo = $_POST['conteudo'];
+
+    //se os post não estiverem vazios
+    if(!empty($id) && !empty($titulo)  && !empty($conteudo)){
+        $n = new Noticia();
+        $n->setId($id);
+        $n->setTitulo($titulo);
+        $n->setConteudo($conteudo);
+        //atualiza os dados da notítica
+        $n->atualiza();
+        header('Location: index.php');
+        }else{
+        echo "vazio";
         }
+    }
 
-        if(isset($_POST['titulo']) && isset($_POST['conteudo'])){
-
-            $id       = $_GET['id'];
-            $titulo   = $_POST['titulo'];
-            $conteudo = $_POST['conteudo'];
-
-            if(!empty($id) && !empty($titulo)  && !empty($conteudo)){
-                $n = new Noticia();
-                $n->setId($id);
-                $n->setTitulo($titulo);
-                $n->setConteudo($conteudo);
-                $n->atualiza();
-                header('Location: index.php');
-            }else{
-                echo "vazio";
-            }
-
-            
-            
-        }
-
-        if(isset($_GET['id_remove'])){
-            echo "remove";
-            $n = new Noticia();
-            $n->setId($_GET['id_remove']);
-            $n->remove();
-            header('Location: index.php');
-        }
-        
+//verifica se o get id_remove exite
+if(isset($_GET['id_remove'])){
+    $n = new Noticia();
+    $n->setId($_GET['id_remove']);
+    //remove a notícia
+    $n->remove();
+    header('Location: index.php');
+}
