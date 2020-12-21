58b7e417864d74d75b417fbfc10b57be4a54d881
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-12-21 12:19:16 -0200

    IndexController.php

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -1,28 +1,26 @@
 <?php
 require_once "App/Models/Noticia.php";  
 
-        $noticias = null;
+$noticias = null;
 
-        $n = new Noticia();
+$n = new Noticia();
 
-        //se existir o metodo get busca
-        if(isset($_GET['busca'])) {
+//se existir o metodo get busca
+if(isset($_GET['busca'])) {
 
-            $pesquisa = $_GET['busca'];
+    $pesquisa = $_GET['busca'];
 
-            //verifica se está vazio
-            if(!empty($pesquisa)){
-                $n->setTitulo($pesquisa);
-                $noticias = $n->pesquisa();
+    //verifica se está vazio
+    if(!empty($pesquisa)){
+        $n->setTitulo($pesquisa);
+        $noticias = $n->pesquisa();
 
-            }else{
-                echo"<script> alert('Porfavor, preencha o campo!') </script>";
-                $noticias = $n->selectNoticias();
-            }
-        
-        // caso não exita o get busca retorna todas as noticias do banco de dados
-        }else{
-            $noticias = $n->selectNoticias();
-        }
-    
+    }else{
+        echo"<script> alert('Porfavor, preencha o campo!') </script>";
+        $noticias = $n->selectNoticias();
+    }
 
+// caso não exita o get busca retorna todas as noticias do banco de dados
+ }else{
+    $noticias = $n->selectNoticias();
+}
