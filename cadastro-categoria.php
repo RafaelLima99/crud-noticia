e0ea002509955c8e2ff6f715db3dd2d1937854a6
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-12-21 12:19:21 -0200

    cadastro-categoria.php

--- cadastro-categoria.php
+++ cadastro-categoria.php
@@ -2,7 +2,7 @@
     require_once "app/controllers/CategoriaController.php";
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,11 +23,7 @@
             </div>
         </div>
     </div>
-    <footer>
-        <div>
-            Todos direitos
-        </div>
-    </footer>
+<?php require_once "app/includes/footer.php";?>    
 </body>
 <script src="assets/js/navbar.js"></script>
 </html>
