5b767988ef4e8f16b0bbe824f7a16cd7b16c79a8
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-12-21 12:19:13 -0200

    CategoriaController.php

--- app/Controllers/CategoriaController.php
+++ app/Controllers/CategoriaController.php
@@ -2,12 +2,15 @@
 
 require_once "app/Models/Categoria.php";
 
+//verifica se o post titulo existe
 if(isset($_POST['titulo'])){
 
     $nome = $_POST['titulo'];
+    //se não estiver vazio
     if(!empty($nome)){
 
         $categoria = new Categoria();
+        //cadastra a categoria
         $categoria->setNome($nome);
 
         if($categoria->cadastra()){
